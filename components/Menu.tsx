e5517418e710dca2dcd0dc1532501f56837ff36a
hamedrf <123485655+hamedrf@users.noreply.github.com>
2024-07-20 12:11:18 +0330

    update

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,27 +1,36 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import $ from "jquery";
 
 export const Menu = () => {
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {
     $(".nav-link").each(function () {
       $(this).attr("title", $(this).text());
     });
-  }, []);
+  });
+
   return (
     <Navbar expand="lg" className="">
       <Container fluid>
         <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="mx-auto my-2 my-lg-0 "
-            style={{ maxHeight: "100px" }}
-            navbarScroll
+        <Navbar.Toggle aria-controls="navbarScroll">
+          <div
+            id="nav-btn-icon"
+            className={open ? "open" : ""}
+            onClick={() => setOpen(!open)}
           >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="mx-auto my-2 my-lg-0" navbarScroll>
             <Nav.Link href="#1">Home</Nav.Link>
             <Nav.Link href="#2">About</Nav.Link>
             <Nav.Link href="#3">Services</Nav.Link>
---
+++ types/jquery.d.ts
@@ -0,0 +1,16 @@
+declare var $: any;
+
+// declare module "jquery" {
+//   interface JQuery {
+//     each(func: (index: number, element: HTMLElement) => void): JQuery;
+//     attr(attributeName: string, value: any): JQuery;
+//     text(): string;
+//   }
+
+//   interface JQueryStatic {
+//     (selector: string): JQuery;
+//   }
+
+//   const $: JQueryStatic;
+//   export = $;
+// }
