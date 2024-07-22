cdf066a322687417211d641967414ae36cabfaf6
hamedrf <123485655+hamedrf@users.noreply.github.com>
2024-07-22 16:25:13 +0330

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,8 @@ const Home = () => {
     <>
       <header className="position-relative">
         <Menu />
-        <div className="d-flex justify-content-around align-items-center row">
-          <div className="bg-danger col-3">1</div>
-          <div className=" col-3">
-            <Image
-              src={hamedrajabifarjad}
-              className=" w-100 h-100 "
-              alt="حامد رجبی فرجاد"
-            />
-          </div>
-        </div>
         <div
-          className=" position-absolute top-0 w-100 z-n1"
-          style={{ height: "40vh" }}
+          className=" position-absolute top-0 w-100 z-n1 svg-header-size"
         >
           <div className="position-relative svg-separator w-100 h-100">
             <svg
@@ -54,6 +43,47 @@ const Home = () => {
               </g>
             </svg>
           </div>
+        </div>
+        <div className="d-flex justify-content-around align-items-center row flex-lg-row-reverse">
+          <div className=" col-lg-4 col-12 position-relative  d-flex justify-content-center">
+            <Image
+              src={hamedrajabifarjad}
+              style={{
+                width: "65%",
+                // transform: "rotate(1deg) translate(0px, -5px);",
+              }}
+              className="  h-100"
+              alt="حامد رجبی فرجاد"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="position-absolute bottom-0 z-n1"
+              style={{ left: "0" }}
+              version="1.0"
+              width="100%"
+              height="100%"
+              viewBox="0 0 367 280"
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <defs>
+                <linearGradient id="grad1" x1="0%" x2="100%" y1="0%" y2="0%">
+                  <stop offset="0%" stop-color="#7091e6" />
+                  <stop offset="100%" stop-color="#3d52a0" />
+                </linearGradient>
+              </defs>
+              <metadata>
+                Created by potrace 1.10, written by Peter Selinger 2001-2011
+              </metadata>
+              <g
+                transform="translate(0,319)  scale(0.1000,-0.1000)"
+                fill="url(#grad1)"
+                stroke="none"
+              >
+                <path d="M2304 2780 c-172 -45 -298 -136 -497 -360 -241 -270 -342 -362 -471 -427 -39 -20 -210 -83 -381 -140 -438 -146 -620 -238 -764 -384 -75 -76 -122 -149 -159 -247 -21 -56 -26 -89 -30 -203 -5 -152 12 -237 67 -347 176 -348 650 -554 1466 -637 341 -34 995 -47 1245 -24 507 46 777 211 867 529 15 55 18 102 18 315 -1 212 -4 270 -23 379 -136 788 -556 1435 -1007 1551 -82 21 -241 19 -331 -5z" />
+              </g>
+            </svg>
+          </div>
+          <div className="bg-danger col-3">1</div>
         </div>
       </header>
     </>
